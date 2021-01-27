240b0c76bae304818c86959494efad7d3c4a1fce
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-27 14:28:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_numbers = []
   count = 0
   while count <= array.length do
-    new_numbers << array[count] ** 2
+    new_numbers.push(array[count] **)
   end
   return new_numbers
 end
